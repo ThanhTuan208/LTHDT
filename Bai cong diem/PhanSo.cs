eea8e7e1be9c884cf81aa35e55a62815d47d1dd6
ThanhTuan208 <161844395+ThanhTuan208@users.noreply.github.com>
2024-03-16 08:54:40 +0700

    Add files via upload

---
+++ Bai cong diem/PhanSo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class PhanSo
+    {
+        private int tuSo;
+        private int mauSo; 
+        public PhanSo(int tu, int mau)
+        {
+            tuSo = tu;
+            mauSo = mau;
+        }   
+        public void Print()
+        {
+            Console.WriteLine($"{tuSo}/{mauSo}");
+        }
+        public void NghichDao()
+        {
+            int temp = tuSo;
+            tuSo = mauSo;
+            mauSo=temp;
+        }
+        private int UCLN ()
+        {
+            int Tong;
+            int newTu =Math.Abs(tuSo);
+            int newMau = Math.Abs(mauSo);
+            while (newTu != 0 && newMau != 0)
+            {
+                if (newTu > newMau)
+                {
+                    newTu = newTu % newMau;
+                }
+                else
+                {
+                    newMau = newMau % newTu;
+                }
+            }
+            return Tong = newTu + newMau;
+        }
+        public void RutGon()
+        {
+            int uocChung = UCLN();
+            tuSo = tuSo / uocChung;
+            mauSo = mauSo/ uocChung;
+        }
+        public PhanSo TinhTong(PhanSo y)
+        {
+            int newTu,newMau;
+            newTu = tuSo * y.mauSo + y.tuSo * mauSo;
+            newMau = mauSo * y.mauSo;
+            PhanSo z = new PhanSo(newTu, newMau);
+            return z;
+        }
+        public PhanSo TinhTru(PhanSo y)
+        {
+            int newTu, newMau;
+            newTu = tuSo * y.mauSo - y.tuSo * mauSo;
+            newMau = mauSo * y.mauSo;
+            PhanSo z = new PhanSo(newTu, newMau);
+            return z;
+        }
+        public PhanSo TinhNhan(PhanSo y)
+        {
+            int newTu, newMau;
+            newTu = tuSo * y.tuSo;
+            newMau = mauSo * y.mauSo;
+            PhanSo z = new PhanSo(newTu, newMau);
+            return z;
+        }
+        public PhanSo TinhChia(PhanSo y)
+        {
+            int newTu, newMau;
+            newTu = tuSo * y.mauSo ;
+            newMau = mauSo * y.tuSo;
+            PhanSo z = new PhanSo(newTu, newMau);
+            return z;
+        }
+    }
+}
---
+++ Bai cong diem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PhanSo y = new PhanSo(3,4);
+            PhanSo x = new PhanSo(2, 4);
+            x.Print();
+            y.Print();
+            PhanSo z = x.TinhTong(y);
+            z.Print();
+            z = x.TinhTru(y);
+            z.Print();
+            z = x.TinhNhan(y);
+            z.Print();
+            z = x.TinhChia(y);
+            z.Print();
+        }
+    }
+}
