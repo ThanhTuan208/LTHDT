644268d1b59b07a1f99a3db36ef53804093a8e52
ThanhTuan208 <161844395+ThanhTuan208@users.noreply.github.com>
2024-03-21 03:29:30 -0700

    Add files via upload

---
+++ time/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace time
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            time time = new time(22,59,59);
+            Console.WriteLine(time.ToString());
+            Console.WriteLine(time.nextSecond().ToString());
+            Console.WriteLine(time.nextprevious().ToString());
+
+        }
+    }
+}
---
+++ time/time.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace time
+{
+    internal class time
+    {
+        private int hour;
+        private int minute;
+        private int second;
+
+        public int Hour { get => hour ; set => hour = value; }
+        public int Minute { get => minute; set => minute = value; }
+        public int Second { get => second; set => second = value; }
+
+        public time(int hour, int minute, int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;           
+        }
+
+        public string ToString()
+        {
+            return $"{hour:00}:{minute:00}:{second:00}";
+        }
+        public time nextSecond()
+        {
+            DateTime date = new DateTime(1,1,1,hour,minute,second).AddSeconds(1);
+            hour = date.Hour;
+            minute = date.Minute;
+            second = date.Second;
+            return this;
+           
+        }
+        public time nextprevious()
+        {
+            DateTime date = new DateTime(1, 1, 1, hour, minute, second).AddSeconds(-2);
+            hour = date.Hour;
+            minute = date.Minute;
+            second = date.Second;
+            return this;
+        }
+    }
+}
